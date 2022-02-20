60c187444f9f5fce8eb060432197ae94c6c21333
Joe Denby <jd744@kent.ac.uk>
2022-02-20 19:29:13 +0000

    Server is not hosted on Clever-Cloud web services. Updated DBManager to reflect the change.

--- src/DBManager.java
+++ src/DBManager.java
@@ -14,19 +14,18 @@ public class DBManager {
     public static void connect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/account?user=root&password=D211723m5");
+            connection = DriverManager.getConnection("jdbc:mysql://becncqh5mhfrujm4nsgt-mysql.services.clever-cloud.com/becncqh5mhfrujm4nsgt?user=utgprzq0wm9n97xk&password=a6LBziz1kFxZ294AX63S");
             statement = connection.createStatement();
             resultSet = statement.executeQuery("SELECT * FROM user");
             
             while(resultSet.next()){
-                System.out.println(resultSet.getString("user_id") + " - " + resultSet.getString("email") + " - " + resultSet.getString("password"));
+                System.out.println(resultSet.getString("user_id") + " - " + resultSet.getString("email") + 
+                    " - " + resultSet.getString("password"));
             }
         
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        
     }
 
 }
