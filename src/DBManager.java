831c4135a2525dc8353abd70ce61621cbc0f8294
Joe Denby <jd744@kent.ac.uk>
2022-02-20 21:16:53 +0000

    Login function now implemented.

--- src/DBManager.java
+++ src/DBManager.java
@@ -9,23 +9,58 @@ public class DBManager {
 
     public static void main(String[] args) {
         connect();
+        if(!isConnected()) {
+            System.out.println("Connection failed ...");
+            return;
+        }
+        System.out.println("Connected.");
+        User x = logIn("jd744@kent.ac.uk", "EndlessNameless1");
+        String s = x.equals(User.NULL) ? "Failed to login" : "Logged success!";
+        System.out.println(s);
     }
 
-    public static void connect() {
+    public static boolean connect() {
+        if(connection != null) return true;
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://becncqh5mhfrujm4nsgt-mysql.services.clever-cloud.com/becncqh5mhfrujm4nsgt?user=utgprzq0wm9n97xk&password=a6LBziz1kFxZ294AX63S");
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM user");
-            
-            while(resultSet.next()){
-                System.out.println(resultSet.getString("user_id") + " - " + resultSet.getString("email") + 
-                    " - " + resultSet.getString("password"));
-            }
+            return true;
         
         } catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
     }
+
+    public static boolean isConnected() {
+        if (connection == null) return false;
+        
+        try {
+            return !connection.isClosed();
+        } catch (SQLException exception) {
+            exception.fillInStackTrace();
+            return false;
+        }
+    }
+
+    public static User logIn (String email, String password) {
+        if(!isConnected()) return User.NULL;
+
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM user where email = " + "'" + email + "'" + " and password = " + "'" + password + "'");
+            
+            while(resultSet.next()) {
+                return new User(resultSet.getString("first_name"), resultSet.getString("last_name"), resultSet.getString("email"), true);
+            }
+
+            return User.NULL;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return User.NULL;
+        }
+    } 
+    
 
 }
